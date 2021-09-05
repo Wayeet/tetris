f5727d8b152a673c77807b54896bc264a253f8cd
Wayeet <kontakt@bjoern-roos.com>
2021-09-05 19:01:51 +0200

    main

--- src/de/bjoern/GUI.java
+++ src/de/bjoern/GUI.java
@@ -1,19 +1,25 @@
 package de.bjoern;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 
-public class GUI {
+public class GUI implements ActionListener {
 	
 	//globals
 	
 	JFrame mainFrame;
 	JPanel panel;
 	JLabel label;
+	JButton checker;
+	JOptionPane pane;
 	
 	
 	
@@ -23,21 +29,29 @@ public class GUI {
 		mainFrame = new JFrame();
 		panel = new JPanel();
 		label = new JLabel("a", SwingConstants.CENTER);
+		checker = new JButton("Systemcheck");
+		
+		pane = new JOptionPane("test");
+		
+		
+		checker.addActionListener(this);
 		
 		
 		
-		label.setText("Hallo");
+		label.setText("Willkommen beim Tetris Installer");
 		
 		
-		panel.setBorder(new EmptyBorder(20, 20, 20 , 20));
+		panel.setBorder(new EmptyBorder(20, 20, 20 ,20));
 		panel.setLayout(new GridLayout(0, 1));
 		//adds
 		panel.add(label);
+		panel.add(checker);
 		//----
 		
 		mainFrame.add(panel, BorderLayout.CENTER);
+		mainFrame.setPreferredSize(new Dimension(800, 500));
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainFrame.setTitle("Our GUI");
+		mainFrame.setTitle("Tetris Installer");
 		mainFrame.pack();
 		mainFrame.setVisible(true);
 		
@@ -48,6 +62,15 @@ public class GUI {
 		
 		
 		new GUI();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		System.out.println("check started...");
+		
+		
+		
 	}
 
 }
